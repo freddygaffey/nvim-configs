4d29801e630a333f8b6f68d1e60de12ad3d5ca32
Fred Gaffey <149978677+freddygaffey@users.noreply.github.com>
2024-10-04 00:38:16 +1000

    all works

--- lua/fred/lazy.lua
+++ lua/fred/lazy.lua
@@ -11,10 +11,12 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-
-require("lazy").setup("fred.plugins")
--- , {
---   change_detection = {
---     notify = false,
---   },
--- })
+require("lazy").setup("fred.plugins", {
+  checker = {
+    enabled = true,
+    notify = false,
+  },
+  change_detection = {
+    notify = false,
+  },
+})
