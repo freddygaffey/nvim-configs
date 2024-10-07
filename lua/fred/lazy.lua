ac3da1cf91725834ad202ffada4ba61f58838b69
Fred Gaffey <149978677+freddygaffey@users.noreply.github.com>
2024-10-07 17:51:53 +1100

    save

--- lua/fred/lazy.lua
+++ lua/fred/lazy.lua
@@ -11,7 +11,7 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup("fred.plugins", {
+require("lazy").setup({ { import = "fred.plugins" }, { import = "fred.plugins.lsp" } }, {
   checker = {
     enabled = true,
     notify = false,
--- lua/fred/plugins/treesitter.lua
+++ lua/fred/plugins/treesitter.lua
@@ -44,7 +44,7 @@ return {
         "c",
         "cpp",
         "python",
-
+        "conf"
       },
       incremental_selection = {
         enable = true,
